5799793b33361a2f18329b49c613968b07a7820c
yume514 <doc25051@grips.ac.jp>
2025-04-27 13:28:11 +0900

    Update Company.sql

--- Company.sql
+++ Company.sql
@@ -1,42 +1,26 @@
-SELECT
-  	a.appln_id
-	, appln_auth
-	, appln_filing_date
-	, appln_filing_year
-	, docdb_family_id
-	, inpadoc_family_id
-	, docdb_family_size
+SELECT 
+    a.appln_id,
+    a.appln_auth,
+    a.appln_filing_date,
+    a.appln_filing_year,
+    a.docdb_family_id,
+    a.inpadoc_family_id,
+    a.docdb_family_size
 FROM tls201_appln a
-JOIN(
-	SELECT
-    	t.appln_id
-	,appln_title
-    	, appln_title_lg
-FROM tls202_appln_title
-) t
-ON a.appln_id = t.appln_id
-JOIN(
-	SELECT
-    	ab.appln_id
-	,appln_abstract
-    	, appln_abstract_lg
-FROM tls203_appln_abstr
-) ab
-ON a.appln_id = ab.appln_id
-JOIN 	(
-SELECT 
-	t207.person_id, 
-    	t207.appln_id
-from tls207_pers_appln
-)  t207
-ON a.appln_id = t207.appln_id
-JOIN 	(
-SELECT 
-  	person_id, 
-    	psn_name, 
-    	person_address, 
-    	person_ctry_code 
-from tls206_person
-where p.psn_name like '%Company_name%';
-)  p
-ON p.person_id = t206.person_id
+JOIN tls202_appln_title t
+    ON a.appln_id = t.appln_id
+JOIN tls203_appln_abstr ab
+    ON a.appln_id = ab.appln_id
+JOIN tls207_pers_appln t207
+    ON a.appln_id = t207.appln_id
+JOIN (
+    SELECT 
+        person_id, 
+        psn_name, 
+        person_address, 
+        person_ctry_code
+    FROM tls206_person
+    WHERE psn_name LIKE '%docomo%'
+) p
+    ON p.person_id = t207.person_id
+;
