a9bfdd20662a96e43a979bdc94ea1dcd9bfe1b8e
yume514 <doc25051@grips.ac.jp>
2025-04-30 15:34:04 +0900

    Create cpc_keywords.sql

---
+++ cpc_keywords.sql
@@ -0,0 +1,8 @@
+SELECT
+	a.appln_id,
+	cpc.cpc_class_symbol
+FROM
+	tls201_appln a
+JOIN tls224_appln_cpc cpc ON cpc.appln_id = a.appln_id
+JOIN tls203_appln_abstr ab ON ab.appln_id = a.appln_id
+WHERE ab.appln_abstract LIKE '%SLAM%';
