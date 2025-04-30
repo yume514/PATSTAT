f8a231eda760a5c612f237679597a08a5e218c64
yume514 <doc25051@grips.ac.jp>
2025-04-30 15:38:18 +0900

    Update cpc_keywords.sql

--- cpc_keywords.sql
+++ cpc_keywords.sql
@@ -5,4 +5,4 @@ FROM
 	tls201_appln a
 JOIN tls224_appln_cpc cpc ON cpc.appln_id = a.appln_id
 JOIN tls203_appln_abstr ab ON ab.appln_id = a.appln_id
-WHERE ab.appln_abstract LIKE '%key-word%';
+WHERE ab.appln_abstract LIKE '%keywords%';
