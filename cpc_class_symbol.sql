ad2ca5d650b7b33621fd3d08ccd060bb21b87ced
yume514 <doc25051@grips.ac.jp>
2025-06-16 23:34:36 +0900

    Create cpc_class_symbol.sql

---
+++ cpc_class_symbol.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT 
+	a.appln_id,
+    appln_title,
+    appln_title_lg,
+    appln_abstract,
+    appln_abstract_lg,
+    a.appln_auth,
+    a.appln_filing_date,
+    a.appln_filing_year,
+    a.docdb_family_id,
+    a.inpadoc_family_id,
+    a.docdb_family_size
+FROM tls201_appln a
+JOIN tls202_appln_title t ON a.appln_id = t.appln_id
+JOIN tls203_appln_abstr ab ON a.appln_id = ab.appln_id
+JOIN tls224_appln_cpc cpc ON a.appln_id = cpc.appln_id
+WHERE REPLACE(cpc.cpc_class_symbol, ' ', '') LIKE 'G01C21/20%'; -- CPCコードに一致
